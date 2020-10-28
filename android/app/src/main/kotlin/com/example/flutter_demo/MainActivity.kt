4a33a940ceb1224dbccd8419c210473c86dd4053
peace <peace_su@163.com>
2020-10-28 14:54:59 +0800

    解决local_auth0.6.3+3报错问题： 1、升级gradle版本 2、修改主题为：android:theme="@style/Theme.AppCompat"

--- android/app/src/main/kotlin/com/example/flutter_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_demo/MainActivity.kt
@@ -1,25 +1,14 @@
 package com.example.flutter_demo
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
-
-
-// package com.example.flutter_demo
-
-// import io.flutter.embedding.android.FlutterFragmentActivity;
-// import io.flutter.embedding.engine.FlutterEngine;
-
-// import io.flutter.plugins.GeneratedPluginRegistrant;
-
-// import android.os.Build;
-// import android.view.ViewTreeObserver;
-// import android.view.WindowManager;
-// import androidx.annotation.NonNull;
-
-// class MainActivity : FlutterFragmentActivity() {
-//    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-//        GeneratedPluginRegistrant.registerWith(flutterEngine)
-//    }
-// } 
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import android.os.Build;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+import androidx.annotation.NonNull;
+class MainActivity : FlutterFragmentActivity() {
+   override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+       GeneratedPluginRegistrant.registerWith(flutterEngine)
+   }
+} 
